8fe02debd282a8372986744443b9fa22f4f40015
james <james@DESKTOP-JDJA6NK>
2020-07-28 00:23:13 +0100

    Fix relative link conversion code

--- src/SuperMemoAssistant.Plugins.MouseoverCompHope/UrlUtils.cs
+++ src/SuperMemoAssistant.Plugins.MouseoverCompHope/UrlUtils.cs
@@ -20,29 +20,13 @@ namespace SuperMemoAssistant.Plugins.MouseoverCompHope
             baseUrl = baseUrl.TrimEnd('/');
           }
 
-          if (relUrl.StartsWith("/") && !relUrl.StartsWith("//"))
-          {
-            if (relUrl.StartsWith("/wiki") || relUrl.StartsWith("/w/"))
-            {
-              return $"{baseUrl}{relUrl}";
-            }
-            return $"{baseUrl}/wiki{relUrl}";
-          }
-          else if (relUrl.StartsWith("./"))
-          {
-            if (relUrl.StartsWith("./wiki") || relUrl.StartsWith("./w/"))
-            {
-              return $"{baseUrl}{relUrl.Substring(1)}";
-            }
-            return $"{baseUrl}/wiki{relUrl.Substring(1)}";
-          }
-          else if (relUrl.StartsWith("#"))
-          {
-            return $"{baseUrl}/wiki/{relUrl}";
-          }
-          else if (relUrl.StartsWith("//"))
+          if (relUrl.StartsWith("//"))
           {
             return $"https:{relUrl}";
+          }
+          else
+          {
+            return $"{baseUrl}{relUrl}";
           }
         }
       }
